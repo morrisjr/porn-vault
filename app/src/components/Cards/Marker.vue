8cbcfbc123353901644999bcd13e1b49c312c4b5
Christopher Caroni <christophercaroni97@gmail.com>
2021-05-01 16:53:22 +0200

    refactor(Marker): move fav/bookmark buttons

--- app/src/components/Cards/Marker.vue
+++ app/src/components/Cards/Marker.vue
@@ -6,6 +6,29 @@
           <div class="corner-actions top-right" style="z-index: 6">
             <slot name="action" :hover="hover"></slot>
           </div>
+
+          <div class="corner-actions bottom-left" style="z-index: 6">
+            <v-btn
+              light
+              class="elevation-2 mr-1"
+              @click.stop.prevent="favorite"
+              icon
+              style="background: #fafafa"
+            >
+              <v-icon :color="value.favorite ? 'red' : undefined">{{
+                value.favorite ? "mdi-heart" : "mdi-heart-outline"
+              }}</v-icon>
+            </v-btn>
+            <v-btn
+              light
+              class="elevation-2"
+              @click.stop.prevent="bookmark"
+              icon
+              style="background: #fafafa"
+            >
+              <v-icon>{{ value.bookmark ? "mdi-bookmark-check" : "mdi-bookmark-outline" }}</v-icon>
+            </v-btn>
+          </div>
         </v-img>
       </v-hover>
     </a>
@@ -29,25 +52,6 @@
       >
         From scene
         <router-link :to="`/scene/${value.scene._id}`">{{ value.scene.name }}</router-link>
-      </div>
-
-      <div class="mt-2">
-        <v-btn
-          @click="favorite"
-          text
-          small
-          :color="value.favorite ? 'red' : ''"
-          class="text-none mr-2"
-          >Favorite</v-btn
-        >
-        <v-btn
-          @click="bookmark"
-          text
-          small
-          :color="value.bookmark ? 'primary' : ''"
-          class="text-none"
-          >Bookmark</v-btn
-        >
       </div>
 
       <Rating @change="rate" class="my-2 ml-3" :value="value.rating" />
@@ -163,6 +167,11 @@ export default class SceneCard extends Vue {
   &.top-right {
     right: 2px;
     top: 2px;
+  }
+
+  &.bottom-left {
+    bottom: 2px;
+    left: 2px;
   }
 }
 </style>
